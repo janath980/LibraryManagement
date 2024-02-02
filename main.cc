e8858d0bfce20c7bfd512f85ad3991cda5e58fec
thenujad <dulwanat@gmail.com>
2024-02-02 21:55:03 +0530

    Finish backed

--- main.cc
+++ main.cc
@@ -8,18 +8,35 @@ void clearCin() {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard the line
 }
 
+void printHeader() {
+    std::cout << R"(
+  _     _ _                                _   _                                      
+ | |   (_) |                              | | (_)                                     
+ | |    _| |__  _ __ __ _ _ __ _   _    __| |  _ _ __   __ _                          
+ | |   | | '_ \| '__/ _` | '__| | | |  / _` | | | '_ \ / _` |                         
+ | |___| | |_) | | | (_| | |  | |_| | | (_| | | | | | | (_| |                         
+ |______|_|_.__/|_|  \__,_|_|   \__, |  \__,_| |_|_| |_|\__, |                         
+                                __/ |                    __/ |                         
+                               |___/                    |___/                          
+
+)" << '\n';
+}
+
 int main() {
     System librarySystem;
     int choice;
 
     do {
-        std::cout << "\nLibrary Management System\n";
+        printHeader();
+        std::cout << "Library Management System\n";
+        std::cout << "--------------------------------\n";
         std::cout << "1. Create Book\n";
         std::cout << "2. Show Book\n";
         std::cout << "3. Create Student\n";
         std::cout << "4. Issue Book\n";
         std::cout << "5. Return Book\n";
         std::cout << "6. Exit\n";
+        std::cout << "--------------------------------\n";
         std::cout << "Enter your choice: ";
         std::cin >> choice;
         clearCin(); // Clear cin to avoid issues with getline
@@ -29,6 +46,7 @@ int main() {
 
         switch (choice) {
             case 1: {
+                std::cout << "\n-- Create New Book --\n";
                 std::cout << "Enter book number: ";
                 std::cin >> bno;
                 std::cout << "Enter quantity: ";
@@ -44,12 +62,14 @@ int main() {
                 break;
             }
             case 2:
+                std::cout << "\n-- Show Book Details --\n";
                 std::cout << "Enter book number: ";
                 std::cin >> bno;
                 clearCin();
                 librarySystem.showBook(bno);
                 break;
             case 3: {
+                std::cout << "\n-- Register New Student --\n";
                 std::cout << "Enter student name: ";
                 getline(std::cin, sname);
                 std::cout << "Enter student admission number: ";
@@ -59,6 +79,7 @@ int main() {
                 break;
             }
             case 4:
+                std::cout << "\n-- Issue Book to Student --\n";
                 std::cout << "Enter student admission number: ";
                 std::cin >> admno;
                 std::cout << "Enter book number: ";
@@ -67,6 +88,7 @@ int main() {
                 librarySystem.issueBook(admno, bno);
                 break;
             case 5:
+                std::cout << "\n-- Return Book --\n";
                 std::cout << "Enter student admission number: ";
                 std::cin >> admno;
                 std::cout << "Enter book number: ";
@@ -75,10 +97,10 @@ int main() {
                 librarySystem.returnBook(admno, bno);
                 break;
             case 6:
-                std::cout << "Exiting...\n";
+                std::cout << "\nExiting...\n";
                 break;
             default:
-                std::cout << "Invalid choice, please try again.\n";
+                std::cout << "\nInvalid choice, please try again.\n";
         }
     } while (choice != 6);
 
